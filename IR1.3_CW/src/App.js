14ff94dd5681e288c46a090c6fb1e9e1cad48771
Shubham Mane <shubhammane7096@gmail.com>
2026-07-19 12:12:17 +0530

    Learn react with examples

---
+++ IR1.3_CW/src/App.js
@@ -0,0 +1,51 @@
+import "./styles.css";
+
+// props
+
+const Greeting = (props) => {
+  return <h1>Hello, {props.name}</h1>
+}
+
+const UserDetail = (props) => {
+  return (
+    <div>
+      <h1>User Details</h1>
+      <p>Name: {props.name}</p>
+      <p>Age: {props.age}</p>
+      <p>Country: {props.country}</p>
+    </div>
+  )
+}
+
+const WeatherInfo = (props) => {
+  return(
+    <div>
+      <h1>Weather Info</h1>
+      <p>Temperature: {props.temperature} degree celsius</p>
+      <p>Condition: {props.condition}</p>
+    </div>
+  )
+}
+
+const UserAvatar = (props) => {
+  return (
+    <div>
+      <h1>User Avatar</h1>
+      <img src={props.img} alt={props.alt} />
+    </div>
+  )
+}
+
+export default function App() {
+  return (
+    <div>
+      <Greeting name="John" />
+      
+      <UserDetail name="Bob" age={20} country="India"/>
+
+      <WeatherInfo temperature={25} condition="Sunny" />
+
+      <UserAvatar img="https://placehold.co/150" alt="placehold" />
+    </div>
+  );
+}
