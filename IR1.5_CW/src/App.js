0da882eb8d140342b6a4137bd01a473be3451548
Shubham Mane <shubhammane7096@gmail.com>
2026-07-21 11:27:50 +0530

    Learn react with examples

---
+++ IR1.5_CW/src/App.js
@@ -0,0 +1,52 @@
+import "./styles.css";
+
+const Addition = () => {
+  const a = 2
+  const b = 5
+  return(
+    <p>The result of {a} + {b} is {a + b}</p>
+  )
+}
+
+const TemperatureCalculator = () => {
+  const celsius = 30
+  const fahrenheit = (celsius * 9) / 5 + 32
+  return(
+    <p>{celsius} degree celsius is equal to {fahrenheit} Fahrenheit</p>
+  )
+}
+
+const CombineOperations = () => {
+  const a = 15
+  const b = 7
+  const c = 3
+  return(
+    <div>
+      <p>{a} + {b} + {c} equals {a + b + c}</p>
+      <p>{a} - {b} equals {a - b}</p>
+      <p>{a} * {b} equals {a * b}</p>
+    </div>
+  )
+}
+
+const Welcome = () => {
+  const firstName = "John"
+  const lastName = "Doe"
+  return(
+    <p>{`Welcome, ${firstName} ${lastName}`}</p>
+  )
+}
+
+export default function App() {
+  return (
+    <div >
+      <Addition />
+
+      <TemperatureCalculator />
+
+      <CombineOperations />
+
+      <Welcome />
+    </div>
+  );
+}
